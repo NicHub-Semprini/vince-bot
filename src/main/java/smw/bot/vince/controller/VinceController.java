b378d6514cd8bab4751de1f46aff7f180962d69d
n.semprini <n.semprini@192.168.56.1>
2020-04-30 19:40:18 +0200

    Esposto endpoint di test

--- src/main/java/smw/bot/vince/controller/VinceController.java
+++ src/main/java/smw/bot/vince/controller/VinceController.java
@@ -2,7 +2,11 @@ package smw.bot.vince.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import smw.bot.vince.model.Update;
 
 @RestController
 public class VinceController {
@@ -10,6 +14,11 @@ public class VinceController {
 	@GetMapping("/hello")
 	public ResponseEntity<String> getNextSession(){
 		return ResponseEntity.ok("coming soon");
+	}
+	
+	@PostMapping("/<md5-del-token>/updates")
+	public ResponseEntity<String> updatingWebHook(@RequestBody Update update){
+		return ResponseEntity.ok("messaggio ricevuto :)");
 	}
 
 }
