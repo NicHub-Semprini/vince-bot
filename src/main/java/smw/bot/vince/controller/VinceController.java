7311ef3d3e79b4150eb693c17e3eb6cec5ec6c1d
nicol <nicol@NicoTop>
2020-05-02 16:30:31 +0200

    Implementazione echo

--- src/main/java/smw/bot/vince/controller/VinceController.java
+++ src/main/java/smw/bot/vince/controller/VinceController.java
@@ -1,8 +1,8 @@
 package smw.bot.vince.controller;
 
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +18,7 @@ public class VinceController {
 	
 	private final RestTemplate restTemplate;
 	
-	private final String url = "https://api.telegram.org/bot1001648084:AAHPcuvoo8gN-XAskF3jtsbBTRTQB2GP3x8/sendMessage" 
+	private final String url = "https://api.telegram.org/bot1001648084:AAHPcuvoo8gN-XAskF3jtsbBTRTQB2GP3x8/sendMessage";
 	
 	@Autowired
 	public VinceController(RestTemplate restTemplate) {
@@ -31,10 +31,9 @@ public class VinceController {
 	}
 	
 	@PostMapping(path = "/f29ca76cdbaec82d5819ce9f4a52773f/updates", consumes = "application/json")
-	public <T> ResponseEntity<T> updatingWebHook(@RequestBody Update update){
+	public ResponseEntity<String> updatingWebHook(@RequestBody Update update){
 		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("chat_id", update.getMessage().getChat().getId()).queryParam("text", update.getMessage().getText());
-		restTemplate.postForEntity(url, request, responseType)
-		return ResponseEntity.ok("messaggio ricevuto :)");
+		return restTemplate.postForEntity(builder.build().encode(StandardCharsets.UTF_8).toUriString(), null, String.class);
 	}
 
 }
