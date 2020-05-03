054900c9d8b231c7d25047a9567a6bb9051ddebc
nicol <nicol@192.168.99.1>
2020-05-03 19:12:05 +0200

    Test with keep alive

--- src/main/java/smw/bot/vince/VinceApplication.java
+++ src/main/java/smw/bot/vince/VinceApplication.java
@@ -11,7 +11,7 @@ public class VinceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VinceApplication.class, args);
 		KeepAliveThread kat = new KeepAliveThread();
-		kat.run();
+	    kat.start();
 	}
 	
 }
--- src/main/java/smw/bot/vince/thread/KeepAliveThread.java
+++ src/main/java/smw/bot/vince/thread/KeepAliveThread.java
@@ -1,13 +1,23 @@
 package smw.bot.vince.thread;
 
-public class KeepAliveThread implements Runnable {
+import org.springframework.web.client.RestTemplate;
+
+public class KeepAliveThread extends Thread {
+	
+	private final RestTemplate restTemplate;
+	private final static String url = "https://master-manager.herokuapp.com/hello";
+	
+	public KeepAliveThread() {
+		this.restTemplate = new RestTemplate();
+	}
 
 	@Override
 	public void run() {
 		while (true) {
 			try {
-				Thread.sleep(1500000);
+				Thread.sleep(2000);
 				System.out.println("SVEGLIA!");
+				System.out.println(restTemplate.getForObject(url, String.class));
 			} catch (InterruptedException e) {
 				continue;
 			}
