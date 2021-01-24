96c4dcb80a2256641d72f72a820c7b1f0080b3e7
nicol <nicol@NicoTop>
2021-01-24 15:41:06 +0100

    Increase wake-up GET scheduling to 29 minutes

--- src/main/java/smw/bot/vince/thread/KeepAliveThread.java
+++ src/main/java/smw/bot/vince/thread/KeepAliveThread.java
@@ -5,7 +5,7 @@ import org.springframework.web.client.RestTemplate;
 public class KeepAliveThread extends Thread {
 	
 	private final RestTemplate restTemplate;
-	private final static String url = "https://master-manager.herokuapp.com/hello";
+	private final static String URL = "https://master-manager.herokuapp.com/wake-up";
 	
 	public KeepAliveThread() {
 		this.restTemplate = new RestTemplate();
@@ -15,9 +15,9 @@ public class KeepAliveThread extends Thread {
 	public void run() {
 		while (true) {
 			try {
-				Thread.sleep(1500000); // 25 minuti
+				Thread.sleep(1740000); // 29 minuti
 				System.out.println("SVEGLIA!");
-				System.out.println(restTemplate.getForObject(url, String.class));
+				System.out.println(restTemplate.getForObject(URL, String.class));
 			} catch (InterruptedException e) {
 				continue;
 			}
