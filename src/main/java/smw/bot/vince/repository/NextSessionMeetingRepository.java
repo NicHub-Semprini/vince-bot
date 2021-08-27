9dbdb9820f3c8b06c5fd89fdfbb2f365218dd8ca
n.semprini <n.semprini@blu23200.replynet.prv>
2021-08-27 16:43:30 +0200

    Add @Query.nativeQuery in repository

--- src/main/java/smw/bot/vince/repository/NextSessionMeetingRepository.java
+++ src/main/java/smw/bot/vince/repository/NextSessionMeetingRepository.java
@@ -11,7 +11,7 @@ import smw.bot.vince.model.db.NextSessionMeeting;
 @Repository
 public interface NextSessionMeetingRepository extends CrudRepository<NextSessionMeeting, Long> {
 	
-	@Query(value = "SELECT * FROM next_session_meeting LIMIT 1")
+	@Query(value = "SELECT * FROM next_session_meeting LIMIT 1", nativeQuery = true)
 	Optional<NextSessionMeeting> findFirst();
 
 }
