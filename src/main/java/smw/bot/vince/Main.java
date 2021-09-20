5ea68794db72c8124a74520f993df76801b968ae
nicol <nicol@NicoTop>
2021-09-20 23:40:31 +0200

    Add crypto library <3

--- src/main/java/smw/bot/vince/Main.java
+++ src/main/java/smw/bot/vince/Main.java
@@ -1,6 +1,10 @@
 package smw.bot.vince;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.inject.Inject;
+
+import com.smw.crypto.aes.AESUtils;
 
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
@@ -12,16 +16,31 @@ public class Main {
 
 	public static void main(String ... args) {
         Quarkus.run(App.class, args);
+        try {
+	        SecretKey key = AESUtils.generateKey(AESUtils.KEY_LENGTH_256);
+	        IvParameterSpec iv = AESUtils.generateIv();
+	        String algorithm = "AES/OFB32/PKCS5Padding";
+	        String plain = "falco";
+	        System.out.println("plain = " + plain);
+	        String crypted = AESUtils.encryptToString(algorithm, key, iv, plain);
+	        System.out.println("crypted = " + crypted);
+	        String decrypted = AESUtils.decryptToString(algorithm, key, iv, crypted);
+	        System.out.println("decrypted = " + decrypted);
+	        System.out.println("equals = " + plain.equals(decrypted));
+        } catch(Exception e) {
+        	System.out.println("ECCEZIONE");
+        	e.printStackTrace();
+        }
     }
 	
 	public static class App implements QuarkusApplication {
 
 		@Inject
-		private KeepAliveThread t;
+		KeepAliveThread keepAliveThread;
 		
         @Override
         public int run(String... args) throws Exception {
-        	t.start();
+        	keepAliveThread.start();
             Quarkus.waitForExit();
             return 0;
         }
